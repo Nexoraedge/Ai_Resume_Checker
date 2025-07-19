8cd7f64467682534ebf4ee4a3304be8e2c0dc580
Nexoraedge <onlyego1043@gmail.com>
2025-07-19 23:44:16 +0530

    authentication

---
+++ app/routes/auth.tsx
@@ -0,0 +1,56 @@
+import { useEffect } from "react";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter"
+
+
+export const meta = () => ([
+    {
+        title: "Resume ATS | auth",
+    },
+    {
+        name: "description",
+        content: "Log in to your account"
+    }
+])
+const auth = () => {
+    const {isLoading , auth } = usePuterStore();
+    const location = useLocation();
+    const next = location.search.split("next=")[1];
+    const navigate = useNavigate();
+    useEffect(() => {
+      if(auth.isAuthenticated) {
+        navigate(next || "/");
+      }
+    }, [auth.isAuthenticated,next])
+    
+    return (
+        <main className="bg-[url('/images/bg-main.svg')] min-h-screen flex justify-center items-center bg-cover">
+            <div className="gradient-border shadow-lg">
+                <section className="flex flex-col rounded-2xl gap-8 bg-white p-10">
+                    <div className="flex flex-col items-center text-center gap-2">
+                        <h1>Welcome</h1>
+                        <h2>Log in to continue your Job Journey</h2>
+                    </div>
+                    {isLoading?(
+                        <button className="auth-button animate-pulse">
+                            <p>Singing You in ....</p>
+                        </button> ): (
+                            <>
+                            {auth.isAuthenticated ? (
+                                <button className="auth-button" onClick={auth.signOut}>
+                                    <p>Sign Out</p>
+                                </button>
+                            ) : (
+                                <button className="auth-button" onClick={auth.signIn}>
+                                    <p>Log In</p>
+                                </button> 
+                            )}
+                            </>
+                        )}
+                </section>
+            </div>
+        </main>
+    )
+}
+
+export default auth
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -2,6 +2,9 @@ import type { Route } from "./+types/home";
 import Navbar from "../components/Navbar";
 import { resumes } from "const";
 import Resume_cards from "~/components/Resume_cards";
+import { useLocation, useNavigate } from "react-router";
+import { usePuterStore } from "~/lib/puter";
+import { useEffect } from "react";
 
 
 export function meta({ }: Route.MetaArgs) {
@@ -12,6 +15,16 @@ export function meta({ }: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const {isLoading , auth } = usePuterStore();
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    if(!auth.isAuthenticated) {
+      navigate("/auth?next=/")
+    }
+  }, [auth.isAuthenticated ])
+  
+
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
     <Navbar />
       <section className="main-section md:mx-15 mx-2">
